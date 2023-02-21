4e4bdc8389f98e8f6547f8bd9457e3da814f6a5e
Rafael Costa Palma <rafael.palma@opah.com.br>
2023-02-21 19:26:13 -0300

    first commit

---
+++ src/pages/Transactions/index.tsx
@@ -0,0 +1,40 @@
+import { Header } from "../../components/Header";
+import { Summary } from "../../components/Summary";
+import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
+
+export function Transactions() {
+  return (
+    <div>
+      <Header />
+      <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de website</td>
+              <td>
+                <PriceHighlight variant="income">
+                  R$ 12.000,00
+                </PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/22</td>
+            </tr>
+            <tr>
+              <td width="50%">Hamburger</td>
+              <td>
+                <PriceHighlight variant="outcome">
+                  - R$ 12.000,00
+                </PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>12/04/22</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
+      
+    </div>
+  )
+}
