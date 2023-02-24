08d88dcccbfa18454b8aed0d5107e19e747f93a7
Rafael Costa Palma <rafaelcostapalma@protonmail.com>
2023-02-23 21:08:21 -0300

    adding modal

---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,32 @@
+import * as Dialog from '@radix-ui/react-dialog'
+import { X } from 'phosphor-react'
+import { CloseButton, Content, Overlay } from './styles'
+
+export const NewTransactionModal = () => {
+    return (
+        <Dialog.Portal>
+            <Overlay />
+
+            <Content>
+              <Dialog.Title>Nova transação</Dialog.Title>
+
+              <CloseButton>
+                  <X size={24} />
+              </CloseButton>
+
+              <form action="">
+
+                  <input type="text" placeholder="Descrição" required />
+
+                  <input type="number" placeholder="Preço" />
+
+                  <input type="text" placeholder="Categoria" />
+
+                  <button type="submit">Cadastrar</button>
+
+              </form>
+              
+            </Content>
+        </Dialog.Portal>
+    )
+}
