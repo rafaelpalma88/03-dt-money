b6d5661f0310a525ada5a72472c479b66f785ac6
Rafael Costa Palma <rafaelcostapalma@protonmail.com>
2023-02-27 23:12:44 -0300

    using custom hook

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -2,25 +2,12 @@ import { SummaryContainer, SummaryCard } from './styles'
 import { ArrowCircleUp, ArrowCircleDown, CurrencyDollar } from 'phosphor-react'
 import { useContext, useEffect, useState } from 'react'
 import { TransactionsContext } from '../../context/TransactionsContext'
+import { priceFormatter } from '../../utils/formatter'
+import { useSummary } from '../../hooks/useSummary'
 
 export function Summary() {
 
-  const { transactions } = useContext(TransactionsContext)
-
-  const summary = transactions.reduce((acc, transaction) => {
-    if (transaction.type === 'income') {
-      acc.income += transaction.price;
-      acc.total += transaction.price;
-    } else if (transaction.type === 'outcome') {
-      acc.outcome += transaction.price
-      acc.total += transaction.price;
-    } 
-    return acc;
-  }, {
-    income: 0,
-    outcome: 0,
-    total: 0
-  })
+  const summary = useSummary();
 
   return (
     <SummaryContainer>
@@ -30,7 +17,7 @@ export function Summary() {
           <ArrowCircleUp size={32} color="#00b37e" />
         </header>
 
-        <strong>{summary.income}</strong>
+        <strong>{priceFormatter.format(summary.income)}</strong>
       </SummaryCard>
 
       <SummaryCard>
@@ -39,7 +26,7 @@ export function Summary() {
           <ArrowCircleDown size={32} color="#f75a68" />
         </header>
 
-        <strong>{summary.outcome}</strong>
+        <strong>{priceFormatter.format(summary.outcome)}</strong>
       </SummaryCard>
 
       <SummaryCard variant='green'>
@@ -48,7 +35,7 @@ export function Summary() {
           <CurrencyDollar size={32} color="#fff" />
         </header>
 
-        <strong>{summary.total}</strong>
+        <strong>{priceFormatter.format(summary.total)}</strong>
       </SummaryCard>
     </SummaryContainer>
   )
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -5,6 +5,7 @@ import { SearchForm } from "../../components/SearchForm";
 import { Summary } from "../../components/Summary";
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 import { TransactionsContext } from "../../context/TransactionsContext";
+import { dateFormatter, priceFormatter } from "../../utils/formatter";
 
 interface ITransaction {
   id: number;
@@ -35,11 +36,12 @@ export function Transactions() {
                 <td width="50%">{transaction.description}</td>
                 <td>
                   <PriceHighlight variant={transaction.type}>
-                    {transaction.price}
+                    {transaction.type === 'outcome' && '- '}
+                    {priceFormatter.format(transaction.price)}
                   </PriceHighlight>
                 </td>
                 <td>{transaction.category}</td>
-                <td>{transaction.createdAt}</td>
+                <td>{dateFormatter.format(new Date(transaction.createdAt))}</td>
               </tr>
             ))}
           </tbody>
